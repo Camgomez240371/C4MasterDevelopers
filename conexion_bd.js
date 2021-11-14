4c46c7c1d1ed66da4394560c31bef26452a5bc6c
Camgomez240371 <cristian.gomez-ar@uniminuto.edu.co>
2021-11-14 10:07:10 -0500

    Up first sprint

---
+++ conexion_bd.js
@@ -0,0 +1,11 @@
+
+// Este codigo conecta la base de datos creada en Mongo BD
+const mongourl= "mongodb+srv://JuanCamilo:Bruno0520@proyectociclo4.9livf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+var mongoose = require('mongoose');
+
+mongoose.connect(mongourl, {useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
+    console.log("mongodb is connected");
+}).catch((error)=>{
+    console.log("mondb not connected");
+    console.log(error);
+});
